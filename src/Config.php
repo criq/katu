8c2d7d2dc5ed0dac7f0e47862dfd933480d6b01d
Jan Jablunka <jablunka@gmail.com>
2014-02-19 17:16:01 +0100

    incremental

--- src/Config.php
+++ src/Config.php
@@ -5,10 +5,18 @@ namespace Jabli\Aids;
 class Config {
 
 	static function get() {
-		$config = include rtrim(BASE_DIR) . '/config.php';
+		if (!defined('BASE_DIR')) {
+			throw new Exception("Undefined BASE_DIR.");
+		}
 
+		$filename = rtrim(BASE_DIR) . '/config.php';
+		if (!is_readable($filename)) {
+			throw new Exception("Missing config file at " . $filename . ".");
+		}
+
+		$config = include $filename;
 		if (!is_array($config)) {
-			throw new Exception("Invalid config array.");
+			throw new Exception("Invalid config array for env hash " . Env::getHash() . ".");
 		}
 
 		foreach (func_get_args() as $key) {
