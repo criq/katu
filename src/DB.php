24e905ad1c77c59563353437d716b678910f55d9
Jan Jablunka <jablunka@gmail.com>
2014-02-23 17:41:21 +0100

    incremental

--- src/DB.php
+++ src/DB.php
@@ -8,4 +8,8 @@ class DB {
 		return new \Dabble\Database($config['host'], $config['user'], $config['pswd'], $config['name']);
 	}
 
+	static function getNotORM($config) {
+		return new \NotORM(new \PDO('mysql:dbname=' . $config['name'] . ';host=' . $config['host'], $config['user'], $config['pswd']));
+	}
+
 }
