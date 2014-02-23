bfc2c0da72cd16aa9bded535a1f4928f4a6ac981
Jan Jablunka <jablunka@gmail.com>
2014-02-23 09:17:52 +0100

    incremental

--- src/YAML.php
+++ src/YAML.php
@@ -4,6 +4,12 @@ namespace Jabli\Aids;
 
 class YAML {
 
+	static function respond($var) {
+		header('Content-Type: text/plain; charset=UTF-8');
+
+		return self::encode($var);
+	}
+
 	static function encode($var) {
 		return \Spyc::YAMLDump($var);
 	}
