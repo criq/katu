1ec3b32c8df5bcea7bd406f1ed94cb998e436432
Jan Jablunka <jablunka@gmail.com>
2014-02-24 09:47:40 +0100

    incremental

--- src/Config.php
+++ src/Config.php
@@ -9,9 +9,26 @@ class Config {
 			throw new Exception("Undefined BASE_DIR.");
 		}
 
-		$filename = rtrim(BASE_DIR) . '/config.php';
+		// Try files at different locations.
+		$locations = array(
+			rtrim(BASE_DIR) . '/config.php',
+			rtrim(BASE_DIR) . '/config/init.php',
+		);
+
+		foreach ($locations as $location) {
+			if (file_exists($location)) {
+				$filename = $location;
+				break;
+			}
+		}
+
+		if (!isset($filename)) {
+			throw new Exception("No config file found.");
+		}
+
+		$filename = rtrim(BASE_DIR) . '/config/init.php';
 		if (!is_readable($filename)) {
-			throw new Exception("Missing config file at " . $filename . ".");
+			throw new Exception("Unable to read config file at " . $filename . ".");
 		}
 
 		$config = include $filename;
@@ -28,6 +45,23 @@ class Config {
 		}
 
 		return $config;
+	}
+
+	static function getSpec() {
+		$specific = BASE_DIR . '/config/' . func_get_arg(0) . '.php';
+		if (is_readable($specific)) {
+			$config = include $specific;
+
+			foreach (array_slice(func_get_args(), 1) as $key) {
+				if (isset($config[$key])) {
+					$config = $config[$key];
+				} else {
+					throw new Exception("Invalid config key.");
+				}
+			}
+
+			return $config;
+		}
 	}
 
 }
