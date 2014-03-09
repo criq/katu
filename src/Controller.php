6b08f254b5202c844d5b0d99475f044d5fde626e
Jan Jablunka <jablunka@gmail.com>
2014-03-09 22:54:05 +0100

    incremental

--- src/Controller.php
+++ src/Controller.php
@@ -7,7 +7,15 @@ use \Jabli\App;
 class Controller {
 
 	static function render($view, $data = array()) {
-		return App::getApp()->render($view . '.php', $data);
+		$loader = new \Twig_Loader_Filesystem('./app/Views/');
+		$twig   = new \Twig_Environment($loader, array(
+			'cache'       => TMP_PATH,
+			'auto_reload' => TRUE,
+		));
+
+		echo $twig->render($view . '.tpl', $data);
+
+		return TRUE;
 	}
 
 }
