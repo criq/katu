f745f0e998948b1724b372e4c26f8bfdd860cb20
Jan Jablunka <jablunka@gmail.com>
2014-03-09 21:07:00 +0100

    incremental

--- src/App.php
+++ src/App.php
@@ -6,13 +6,17 @@ use \Slim\Slim;
 
 class App {
 
-	static function getInstance() {
+	static function getApp() {
 		$app = Slim::getInstance();
 		if (!$app) {
 			$app = new Slim(Config::get('slim'));
 		}
 
 		return $app;
+	}
+
+	static function getDB() {
+		return DB\Connection::getInstance();
 	}
 
 }
