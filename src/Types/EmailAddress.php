d1e314e22ac4aab8ad209deab5f087c673aed025
Jan Jablunka <jablunka@gmail.com>
2014-03-22 17:02:37 +0100

    incremental

---
+++ src/Types/EmailAddress.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jabli\Types;
+
+class EmailAddress {
+
+	public $value;
+
+	public function __construct($value) {
+		if (!self::isValid($value)) {
+			throw new Exception("Invalid e-mail address.");
+		}
+
+		$this->value = (string) (trim($value));
+	}
+
+	static function isValid($value) {
+		$validator = new \Egulias\EmailValidator\EmailValidator();
+
+		return $validator->isValid($value);
+	}
+
+}
