a0027be950a46e6c7beeab4c7e448fcd2fe3cf89
Jan Jablunka <jablunka@gmail.com>
2014-03-22 14:41:39 +0100

    incremental

---
+++ src/Utils/Blabot.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jabli\Utils;
+
+class Blabot {
+
+	static function getList($params = array()) {
+		$params = array_merge(array(
+			'scount'     => 100,
+			'method'     => 'list',
+			'format'     => 'json',
+			'language'   => 'cs',
+			'dictionary' => 1,
+		), $params);
+
+		$arr = JSON::decodeAsArray(Cache::getURL('http://api.blabot.net?' . http_build_query($params), 3600));
+
+		return $arr['blabot']['result'];
+	}
+
+}
