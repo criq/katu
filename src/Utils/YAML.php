3818b802bf7397f7ca36b673f9739aa5478ad7bb
Jan Jablunka <jablunka@gmail.com>
2014-03-22 21:16:18 +0100

    incremental

--- src/Utils/YAML.php
+++ src/Utils/YAML.php
@@ -5,6 +5,8 @@ namespace Jabli\Utils;
 class YAML {
 
 	static function respond($var) {
+		header('Content-Type: text/plain; charset=UTF-8');
+
 		$app = \Jabli\FW::getApp();
 		$app->response->setStatus(200);
 		$app->response->headers->set('Content-Type', 'text/plain; charset=UTF-8');
