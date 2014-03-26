1f5ecbf2b9b945cb5d1b13c68f9f143e17af684a
Jan Jablunka <jablunka@gmail.com>
2014-03-26 12:34:38 +0100

    incremental

--- src/DB/Types/Page.php
+++ src/DB/Types/Page.php
@@ -4,12 +4,19 @@ namespace Jabli\DB\Types;
 
 class Page {
 
-	public $page;
-	public $perpage;
+	public $perpage = 1;
+	public $page    = 1;
 
-	public function __construct($page, $perpage) {
-		$this->page    = $page;
-		$this->perpage = $perpage;
+	public function __construct($perpage = 1, $page = 1) {
+		if (!(int) $perpage) {
+			throw new Exception("Invalid per page.");
+		}
+		if (!(int) $page) {
+			throw new Exception("Invalid page.");
+		}
+
+		$this->perpage = (int) $perpage;
+		$this->page    = (int) $page;
 	}
 
 	public function getLimit() {
