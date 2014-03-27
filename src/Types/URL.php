063ccbd92cda91c949819268d19641e5dbc596ef
Jan Jablunka <jablunka@gmail.com>
2014-03-27 09:59:48 +0100

    incremental

--- src/Types/URL.php
+++ src/Types/URL.php
@@ -18,4 +18,33 @@ class URL {
 		return filter_var(trim($value), FILTER_VALIDATE_URL) !== FALSE;
 	}
 
+	public function addParam($name, $value, $overwrite = TRUE) {
+		$parsed = parse_url($this->value);
+
+		if (!$overwrite && isset($parsed['query'][$name])) {
+			throw new Exception("Query param already exists.");
+		}
+
+		$parsed['query'][$name] = $value;
+
+		return self::buildURL($parsed);
+	}
+
+	static function buildURL($params) {
+
+
+		var_dump($parsed);
+
+		/*
+		scheme - e.g. http
+		host
+		port
+		user
+		pass
+		path
+		query - after the question mark ?
+		fragment - after the hashmark #
+		*/
+	}
+
 }
