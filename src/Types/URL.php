ee29b08edb461da3e93d97eef7014fa0722f0a6d
Jan Jablunka <jablunka@gmail.com>
2014-04-09 09:41:04 +0200

    incremental

--- src/Types/URL.php
+++ src/Types/URL.php
@@ -34,6 +34,39 @@ class URL {
 		return TRUE;
 	}
 
+	static function joinPaths() {
+		return implode('/', array_map(function($i){
+			return trim($i, '/');
+		}, func_get_args()));
+	}
+
+	static function getCurrent() {
+		$app = \Jabli\FW::getApp();
+
+		return $app->request->getUrl() . $app->request->getPath();
+	}
+
+	static function getBase() {
+		return \Jabli\Config::getApp('base_url');
+	}
+
+	static function getSite($uri) {
+		return self::joinPaths(self::getBase(), $uri);
+	}
+
+	static function make($url, $params = array()) {
+		return $url . ($params ? '?' . http_build_query($params) : NULL);
+	}
+
+	static function get2ndLevelDomain($url) {
+		$parsed = parse_url($url);
+		if (!isset($parsed['host'])) {
+			throw new \Jabli\Exception("Invalid URL host.");
+		}
+
+		return implode('.', array_slice(explode('.', $parsed['host']), -2));
+	}
+
 	static function buildURL($parts) {
 		$url = '';
 
