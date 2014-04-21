a42eafa04e395a151eeae772beecd20c2d9c7e5c
Jan Jablunka <jablunka@gmail.com>
2014-04-21 10:34:25 +0200

    incremental

--- src/Doctrine/QueryResult.php
+++ src/Doctrine/QueryResult.php
@@ -9,13 +9,13 @@ class QueryResult {
 
 	public function __construct($query, $page = NULL, $perpage = NULL) {
 		$this->data = $query->getResult();
-		$paginator = new \Doctrine\ORM\Tools\Pagination\Paginator($query);
-
-		$total = count($paginator);
 
 		if (!is_null($page) && !is_null($perpage)) {
+			$paginator = new \Doctrine\ORM\Tools\Pagination\Paginator($query);
+			$total = count($paginator);
 			$this->pagination = new \Katu\Types\TPagination($total, $perpage, $page);
 		} else {
+			$total = count($this->data);
 			$this->pagination = new \Katu\Types\TPagination($total, $total, 1);
 		}
 	}
