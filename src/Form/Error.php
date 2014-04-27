aa7682e1c133872cefd2aa22d286ac8a5e424c4e
Jan Jablunka <jablunka@gmail.com>
2014-04-27 21:23:24 +0200

    incremental

---
+++ src/Form/Error.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Katu\Form;
+
+class Error {
+
+	public $error;
+
+	public function __construct($error) {
+		$this->error = (string) $error;
+	}
+
+}
--- src/Form/Evaluation.php
+++ src/Form/Evaluation.php
@@ -2,6 +2,8 @@
 
 namespace Katu\Form;
 
+use \Katu\Flash;
+
 class Evaluation {
 
 	public $name = NULL;
@@ -11,12 +13,48 @@ class Evaluation {
 		$this->name = $name;
 	}
 
+	public function addError($error) {
+		if ($error instanceof \Exception) {
+			if ($error->getMessage()) {
+				$this->errors[] = new Error($error->getMessage());
+
+				return TRUE;
+			}
+
+			return FALSE;
+		}
+
+		if (trim($error)) {
+			$this->errors[] = new Error($error);
+
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+
 	public function addFieldError($type, $fields, $error) {
-		$this->errors[] = new FieldError($type, $fields, $error);
+		if (trim($error)) {
+			$this->errors[] = new FieldError($type, $fields, $error);
+
+			return TRUE;
+		}
+
+		return FALSE;
 	}
 
 	public function hasErrors() {
 		return (bool) count($this->errors);
+	}
+
+	public function setFlash() {
+		foreach ($this->errors as $error) {
+
+			// Set errors.
+			#Flash::add('formErrors.' . $this->name . )
+			#var_dump($error);
+
+		}
 	}
 
 }
