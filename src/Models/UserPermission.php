a3ebcb70d88168347bac7843de154acd4d60ca55
Jan Jablunka <jablunka@gmail.com>
2014-06-07 16:02:39 +0200

    incremental

---
+++ src/Models/UserPermission.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Katu\Models;
+
+use \Katu\Exception;
+
+class UserPermission extends \Katu\Model {
+
+	const TABLE = 'user_permissions';
+
+	static function create($user, $permission) {
+		if (!self::checkCRUDParams($user, $permission)) {
+			throw new \Exception("Invalid params.");
+		}
+
+		return self::insert(array(
+			'timeCreated' => (string) (\Katu\Utils\DateTime::get()->getDBDatetimeFormat()),
+			'userId'      => (int)    ($user->id),
+			'permission'  => (string) (trim($permission)),
+		));
+	}
+
+	static function make($user, $permission) {
+		return self::getOneOrCreateWithList(array(
+			'userId'     => (int)    ($user->id),
+			'permission' => (string) (trim($permission)),
+		), $user, $permission);
+	}
+
+	static function checkCRUDParams($user, $permission) {
+		if (!$user || !($user instanceof User)) {
+			throw new \Exception("Invalid user.");
+		}
+		if (!trim($permission)) {
+			throw new \Exception("Invalid permission.");
+		}
+
+		return TRUE;
+	}
+
+}
