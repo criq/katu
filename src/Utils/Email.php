3efa12f39b51f08a5ffebb239f3154b2c51fe81d
Jan Jablunka <jablunka@gmail.com>
2014-06-09 15:55:01 +0200

    incremental

--- src/Utils/Email.php
+++ src/Utils/Email.php
@@ -15,20 +15,32 @@ class Email {
 
 	public $headers = array();
 
+	public $template;
+	public $content = array();
+	public $variables = array();
+
 	public function __construct($subject = NULL) {
 		$this->setSubject($subject);
+
+		return $this;
 	}
 
 	public function setSubject($subject) {
 		$this->subject = $subject;
+
+		return $this;
 	}
 
 	public function setText($text) {
 		$this->text = $text;
+
+		return $this;
 	}
 
 	public function setHtml($html) {
 		$this->html = $html;
+
+		return $this;
 	}
 
 	public function setBody($html, $text = NULL) {
@@ -39,42 +51,72 @@ class Email {
 		} else {
 			$this->setText(strip_tags($html));
 		}
+
+		return $this;
 	}
 
 	public function setFromEmailAddress($fromEmailAddress) {
 		$this->fromEmailAddress = $fromEmailAddress;
+
+		return $this;
 	}
 
 	public function setFromName($fromName) {
 		$this->fromName = $fromName;
+
+		return $this;
 	}
 
 	public function setFrom($fromEmailAddress, $fromName = NULL) {
 		$this->setFromEmailAddress($fromEmailAddress);
 		$this->setFromName($fromName);
+
+		return $this;
 	}
 
 	public function addTo($toEmailAddress, $toName = NULL) {
 		$this->to[$toEmailAddress] = $toName;
+
+		return $this;
 	}
 
 	public function addHeader($name, $value) {
 		$this->headers[$name] = $value;
+
+		return $this;
 	}
 
 	public function setReplyTo($emailAddress) {
 		$this->addHeader('Reply-To', $emailAddress);
+
+		return $this;
 	}
 
-	public function sendWithMandrillThroughApi($mandrillApi, $options = array()) {
-		$message = array(
-			'subject'    => $this->subject,
-			'html'       => $this->html,
-			'text'       => $this->text,
-			'from_email' => $this->fromEmailAddress,
-			'from_name'  => $this->fromName,
-			'headers'    => $this->headers,
-		);
+	public function setTemplate($template) {
+		$this->template = $template;
+
+		return $this;
+	}
+
+	public function setContent($name, $value) {
+		$this->content[$name] = $value;
+
+		return $this;
+	}
+
+	public function setVariable($name, $value) {
+		$this->variables[$name] = $value;
+
+		return $this;
+	}
+
+	public function getMessageForMandrill($message) {
+		$message['subject']    = $this->subject;
+		$message['html']       = $this->html;
+		$message['text']       = $this->text;
+		$message['from_email'] = $this->fromEmailAddress;
+		$message['from_name']  = $this->fromName;
+		$message['headers']    = $this->headers;
 
 		foreach ($this->to as $toEmailAddress => $toName) {
 			$message['to'][] = array(
@@ -84,7 +126,43 @@ class Email {
 			);
 		}
 
-		return $mandrillApi->messages->send($message);
+		$message['global_merge_vars'] = $this->getVariablesForMandrill();
+
+		return $message;
+	}
+
+	public function getContentForMandrill() {
+		$content = array();
+
+		foreach ($this->content as $name => $value) {
+			$content[] = array(
+				'name'    => $name,
+				'content' => $value,
+			);
+		}
+
+		return $content;
+	}
+
+	public function getVariablesForMandrill() {
+		$variables = array();
+
+		foreach ($this->variables as $name => $value) {
+			$variables[] = array(
+				'name'    => $name,
+				'content' => $value,
+			);
+		}
+
+		return $variables;
+	}
+
+	public function sendWithMandrillThroughApi($mandrillApi, $message = array()) {
+		if ($this->template) {
+			return $mandrillApi->messages->sendTemplate($this->template, $this->getContentForMandrill(), $this->getMessageForMandrill($message));
+		} else {
+			return $mandrillApi->messages->send($this->getMessageForMandrill($message));
+		}
 	}
 
 }
