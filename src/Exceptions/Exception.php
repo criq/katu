9ae36e69e3fc0567d35adda3a7872f214774f6f0
Jan Jablunka <jablunka@gmail.com>
2014-06-12 19:20:07 +0200

    incremental

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -4,4 +4,11 @@ namespace Katu\Exceptions;
 
 class Exception extends \Exception {
 
+	public $data;
+
+	public function __construct($message, $code = 0, $data = array(), $previous = NULL) {
+		parent::__construct($message, $code, $previous);
+		$this->data = $data;
+	}
+
 }
