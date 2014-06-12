c77b8763dab01d484c0a7beb292cbbed55d92023
Jan Jablunka <jablunka@gmail.com>
2014-06-12 20:42:58 +0200

    incremental

--- src/Exceptions/ArgumentErrorException.php
+++ src/Exceptions/ArgumentErrorException.php
@@ -4,4 +4,11 @@ namespace Katu\Exceptions;
 
 class ArgumentErrorException extends ErrorException {
 
+	public $argument;
+
+	public function __construct($message, $argument = NULL, $code = 0, $data = array(), $previous = NULL) {
+		parent::__construct($message, $code, $data, $previous);
+		$this->argument = $argument;
+	}
+
 }
