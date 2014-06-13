c1bec67714799bf04d518c394bfbd506a396912d
Jan Jablunka <jablunka@gmail.com>
2014-06-13 23:34:56 +0200

    incremental

--- src/Exceptions/ModelNotFoundException.php
+++ src/Exceptions/ModelNotFoundException.php
@@ -4,4 +4,8 @@ namespace Katu\Exceptions;
 
 class ModelNotFoundException extends NotFoundException {
 
+	public function __construct($message = '', $code = 0, $context = array(), $previous = NULL) {
+		parent::__construct($message, $code, $context, $previous);
+	}
+
 }
--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -4,4 +4,8 @@ namespace Katu\Exceptions;
 
 class UnauthorizedException extends Exception {
 
+	public function __construct($message = '', $code = 0, $context = array(), $previous = NULL) {
+		parent::__construct($message, $code, $context, $previous);
+	}
+
 }
