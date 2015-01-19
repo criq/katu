80aa9b8d19b8e6727bfab74d742e5357875bbda1
Jan Jablunka <jablunka@gmail.com>
2015-01-19 22:52:21 +0100

    incremental

--- src/Utils/Image.php
+++ src/Utils/Image.php
@@ -120,9 +120,6 @@ class Image {
 			try {
 				$image = \Intervention\Image\Image::make($source);
 			} catch (\Exception $e) {
-				var_dump($e); die;
-				\Katu\ErrorHandler::log($e);
-
 				return false;
 			}
 
