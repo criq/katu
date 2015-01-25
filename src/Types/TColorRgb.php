659d124e6e658d04fa1869288e9dae5d59554bc1
Jan Jablunka <jablunka@gmail.com>
2015-01-25 14:24:25 +0100

    incremental

--- src/Types/TColorRgb.php
+++ src/Types/TColorRgb.php
@@ -26,4 +26,16 @@ class TColorRgb {
 		return round(($this->r + $this->g + $this->b) / 3);
 	}
 
+	public function getHex($prependHash = true) {
+		return ($prependHash ? '#' : null) . implode([
+			str_pad(dechex($this->r), 2, '0', STR_PAD_LEFT),
+			str_pad(dechex($this->g), 2, '0', STR_PAD_LEFT),
+			str_pad(dechex($this->b), 2, '0', STR_PAD_LEFT),
+		]);
+	}
+
+	public function getPhpColor() {
+		return new \phpColors\Color($this->getHex());
+	}
+
 }
