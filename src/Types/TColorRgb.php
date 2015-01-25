9fef75590adf0c1a07661896919231d90e94850c
Jan Jablunka <jablunka@gmail.com>
2015-01-25 16:27:52 +0100

    incremental

--- src/Types/TColorRgb.php
+++ src/Types/TColorRgb.php
@@ -26,6 +26,22 @@ class TColorRgb {
 		return round(($this->r + $this->g + $this->b) / 3);
 	}
 
+	static function getFromHex($hex) {
+		$hex = ltrim($hex, '#');
+
+		if (strlen($hex) == 3) {
+			$r = hexdec(substr($hex, 0, 1) . substr($hex, 0, 1));
+			$g = hexdec(substr($hex, 1, 1) . substr($hex, 1, 1));
+			$b = hexdec(substr($hex, 2, 1) . substr($hex, 2, 1));
+		} else {
+			$r = hexdec(substr($hex, 0, 2));
+			$g = hexdec(substr($hex, 2, 2));
+			$b = hexdec(substr($hex, 4, 2));
+		}
+
+		return new static($r, $g, $b);
+	}
+
 	public function getHex($prependHash = true) {
 		return ($prependHash ? '#' : null) . implode([
 			str_pad(dechex($this->r), 2, '0', STR_PAD_LEFT),
