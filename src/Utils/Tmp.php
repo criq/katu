f9804062b3eeae43a2bd67fbb8ae7bd2092b9e43
Jan Jablunka <jablunka@gmail.com>
2015-02-02 11:40:18 +0100

    incremental

--- src/Utils/Tmp.php
+++ src/Utils/Tmp.php
@@ -7,14 +7,14 @@ class Tmp {
 	static function set($name, $value) {
 		$path = static::getPath($name);
 		@mkdir(dirname($path), 0777, true);
-		file_put_contents($path, $value);
+		file_put_contents($path, serialize($value));
 
 		return $path;
 	}
 
 	static function get($name) {
 		if (file_exists(static::getPath($name))) {
-			return file_get_contents(static::getPath($name));
+			return unserialize(file_get_contents(static::getPath($name)));
 		}
 
 		return null;
