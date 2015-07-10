aac86ce64939f65f5291154515bfb1975b50b1d0
Jan Jablunka <jablunka@gmail.com>
2015-07-10 14:12:17 +0200

    incremental

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -4,7 +4,7 @@ namespace Katu\Exceptions;
 
 class UnauthorizedException extends Exception {
 
-	public function __construct($message = '', $code = 0, $context = array(), $previous = null) {
+	public function __construct($message = 'Unauthorized.', $code = 0, $context = array(), $previous = null) {
 		parent::__construct($message, $code, $context, $previous);
 	}
 
