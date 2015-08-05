72eafac17676a35dc80b079d72297405f171d451
Jan Jablunka <jablunka@gmail.com>
2015-08-05 14:00:26 +0200

    incremental

--- src/Model.php
+++ src/Model.php
@@ -214,7 +214,7 @@ class Model extends ModelBase {
 	static function getIdColumnName() {
 		$table = static::getTable();
 
-		return \Katu\Utils\Cache::getFromMemory(['databases', $table->pdo->name, 'tables', 'idColumn', $table->name->name], function() use($table) {
+		return Utils\Cache::getFromMemory(['databases', $table->pdo->name, 'tables', 'idColumn', $table->name->name], function() use($table) {
 
 			foreach ($table->pdo->createQuery(" DESCRIBE " . $table)->getResult() as $row) {
 				if (isset($row['Key']) && $row['Key'] == 'PRI') {
@@ -259,7 +259,7 @@ class Model extends ModelBase {
 		};
 
 		if (static::CACHE_IN_MEMORY_BY_PRIMARY_KEY) {
-			return \Katu\Utils\Cache::getFromMemory(['model', 'get'], $callback, static::getClass(), $primaryKey);
+			return Utils\Cache::getFromMemory(['model', 'get'], $callback, static::getClass(), $primaryKey);
 		} else {
 			return call_user_func_array($callback, [static::getClass(), $primaryKey]);
 		}
