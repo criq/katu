67881b2dbfd577f98d1373e99a726bdbaa8d7a8a
Jan Jablunka <jablunka@gmail.com>
2016-02-03 11:10:31 +0100

    incremental

--- src/Utils/Cache.php
+++ src/Utils/Cache.php
@@ -19,6 +19,8 @@ class Cache {
 
 		$path = static::getPath((array) $name, (array) $args);
 
+		var_dump($path); die;
+
 		$cache = new \Gregwar\Cache\Cache;
 		$cache->setCacheDirectory(static::getCacheDir($path));
 		$cache->setPrefixSize(0);
