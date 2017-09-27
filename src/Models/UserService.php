5443b5bd381f258dff29043ac0e71b0885eea9b8
Jan Jablunka <jablunka@gmail.com>
2017-09-27 15:42:22 +0200

    incremental

--- src/Models/UserService.php
+++ src/Models/UserService.php
@@ -9,19 +9,19 @@ class UserService extends \Katu\Model {
 	const TABLE = 'user_services';
 
 	static function create($user, $serviceName, $serviceUserId) {
-		return static::insert(array(
-			'timeCreated'   => (string) (\Katu\Utils\DateTime::get()->getDbDateTimeFormat()),
-			'userId'        => (int)    ($user->getId()),
-			'serviceName'   => (string) ($serviceName),
-			'serviceUserId' => (string) ($serviceUserId),
-		));
+		return static::insert([
+			'timeCreated'   => (string)(new \Katu\Utils\DateTime),
+			'userId'        => (int)$user->getId(),
+			'serviceName'   => (string)$serviceName,
+			'serviceUserId' => (string)$serviceUserId,
+		]);
 	}
 
 	static function getByServiceAndId($serviceName, $serviceUserId) {
-		return static::getBy(array(
-			'serviceName'   => (string) ($serviceName),
-			'serviceUserId' => (string) ($serviceUserId),
-		));
+		return static::getBy([
+			'serviceName'   => (string)$serviceName,
+			'serviceUserId' => (string)$serviceUserId,
+		]);
 	}
 
 	static function getOneByServiceAndId($serviceName, $serviceUserId) {
