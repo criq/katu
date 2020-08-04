69aa1dd6641870fa34fb647fe6a1f0cf097f470e
Jan Jablunka <jablunka@gmail.com>
2020-08-04 17:11:31 +0200

    incremental

--- src/Tools/Locks/Lock.php
+++ src/Tools/Locks/Lock.php
@@ -74,7 +74,7 @@ class Lock
 
 	public function getFile()
 	{
-		return new \Katu\Files\File(\Katu\App::getTemporaryDir(), static::DIR_NAME, \Katu\Files\File::generatePath($this->name, 'txt'));
+		return new \Katu\Files\File(\Katu\App::getTemporaryDir(), static::DIR_NAME, \Katu\Files\File::generatePath($this->name, 'lock'));
 	}
 
 	public function isLocked()
