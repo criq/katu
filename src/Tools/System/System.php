bbaf7bc1d5abb55e25ea9cd85226b59c9bc2cdae
Jan Jablunka <jablunka@gmail.com>
2020-09-15 10:53:21 +0200

    incremental

--- src/Tools/System/System.php
+++ src/Tools/System/System.php
@@ -6,32 +6,36 @@ class System
 {
 	public static function getNumberOfCpus() : int
 	{
-		return \Katu\Cache\General::get(['system', 'numberOfCpus'], 86400, function () {
-			$numCpus = 1;
-			if (is_file('/proc/cpuinfo')) {
-				$cpuinfo = file_get_contents('/proc/cpuinfo');
-				preg_match_all('/^processor/m', $cpuinfo, $matches);
-				$numCpus = count($matches[0]);
-			} elseif ('WIN' == strtoupper(substr(PHP_OS, 0, 3))) {
-				$process = @popen('wmic cpu get NumberOfCores', 'rb');
-				if (false !== $process) {
-					fgets($process);
-					$numCpus = intval(fgets($process));
-					pclose($process);
-				}
-			} else {
-				$process = @popen('sysctl -a', 'rb');
-				if (false !== $process) {
-					$output = stream_get_contents($process);
-					preg_match('/hw.ncpu: (\d+)/', $output, $matches);
-					if ($matches) {
-						$numCpus = intval($matches[1][0]);
-					}
-					pclose($process);
-				}
-			}
+		$cacheName = ['system', 'numberOfCpus'];
 
-			return $numCpus;
+		return \Katu\Cache\General::get($cacheName, function () use ($cacheName) {
+			return \Katu\Cache\General::get($cacheName, 86400, function () {
+				$numCpus = 1;
+				if (is_file('/proc/cpuinfo')) {
+					$cpuinfo = file_get_contents('/proc/cpuinfo');
+					preg_match_all('/^processor/m', $cpuinfo, $matches);
+					$numCpus = count($matches[0]);
+				} elseif ('WIN' == strtoupper(substr(PHP_OS, 0, 3))) {
+					$process = @popen('wmic cpu get NumberOfCores', 'rb');
+					if (false !== $process) {
+						fgets($process);
+						$numCpus = intval(fgets($process));
+						pclose($process);
+					}
+				} else {
+					$process = @popen('sysctl -a', 'rb');
+					if (false !== $process) {
+						$output = stream_get_contents($process);
+						preg_match('/hw.ncpu: (\d+)/', $output, $matches);
+						if ($matches) {
+							$numCpus = intval($matches[1][0]);
+						}
+						pclose($process);
+					}
+				}
+
+				return $numCpus;
+			});
 		});
 	}
 
