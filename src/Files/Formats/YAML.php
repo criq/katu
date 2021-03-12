e44f41747092058bc145e22701b7013ab4462e39
Jan Jablunka <jablunka@gmail.com>
2021-03-12 12:03:05 +0100

    incremental

--- src/Files/Formats/YAML.php
+++ src/Files/Formats/YAML.php
@@ -4,15 +4,6 @@ namespace Katu\Files\Formats;
 
 class YAML
 {
-	// static function respond($var) {
-	// 	$app = \Katu\App::get();
-
-	// 	$app->response->headers->set('Content-Type', 'text/plain; charset=UTF-8');
-	// 	$app->response->setBody(self::encode($var));
-
-	// 	return true;
-	// }
-
 	public static function encode($var)
 	{
 		return \Spyc::YAMLDump($var);
