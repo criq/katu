bb2e011dddc26fe73cd6c95db4a80abd274913cd
Jan Jablunka <jablunka@gmail.com>
2022-04-29 10:14:34 +0200

    incremental

--- src/Tools/Services/Google/UrlShortener.php
+++ src/Tools/Services/Google/UrlShortener.php
@@ -7,22 +7,22 @@ use Katu\Types\TIdentifier;
 
 class UrlShortener
 {
-	public static function getApiKey() : string
+	public static function getApiKey(): string
 	{
-		return \Katu\Config\Config::get('google', 'api', 'key');
+		return \Katu\Config\Config::get("google", "api", "key");
 	}
 
-	public static function shorten($url, Timeout $timeout) : ?string
+	public static function shorten($url, Timeout $timeout): ?string
 	{
 		return \Katu\Cache\General::get(new TIdentifier(__CLASS__, __FUNCTION__, __LINE__), $timeout, function ($url) {
-			$apiUrl = \Katu\Types\TURL::make('https://www.googleapis.com/urlshortener/v1/url', [
-				'key' => static::getApiKey(),
+			$apiUrl = \Katu\Types\TURL::make("https://www.googleapis.com/urlshortener/v1/url", [
+				"key" => static::getApiKey(),
 			]);
 
 			$curl = new \Curl\Curl;
-			$curl->setHeader('Content-Type', 'application/json');
+			$curl->setHeader("Content-Type", "application/json");
 			$res = $curl->post($apiUrl, \Katu\Files\Formats\JSON::encode([
-				'longUrl' => (string) $url,
+				"longUrl" => (string) $url,
 			]));
 
 			if (isset($res->id)) {
