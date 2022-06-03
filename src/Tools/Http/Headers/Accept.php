ce09f6bc378a72d76294fa930273e3e3f837a0e6
Jan Jablunka <jablunka@gmail.com>
2022-06-03 13:52:30 +0200

    incremental

--- src/Tools/Http/Headers/Accept.php
+++ src/Tools/Http/Headers/Accept.php
@@ -6,7 +6,7 @@ class Accept extends \Katu\Tools\Http\Headers
 {
 	public static function createFromRequest(\Slim\Http\Request $request)
 	{
-		return new static($request->getHeader('accept'));
+		return new static($request->getHeader("accept"));
 	}
 
 	public function accepts(string $mime)
@@ -18,8 +18,8 @@ class Accept extends \Katu\Tools\Http\Headers
 	{
 		$res = [];
 		foreach ($this as $header) {
-			foreach (explode(',', $header) as $i) {
-				list($mime, $q) = array_pad(explode(';', $i), 2, null);
+			foreach (explode(",", $header) as $i) {
+				list($mime, $q) = array_pad(explode(";", $i), 2, null);
 				$res[] = new AcceptMime($mime, $q);
 			}
 		}
