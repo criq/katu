9c5068850fe5f9bef3ef500a5de134d78d5e528e
Jan Jablunka <jablunka@gmail.com>
2022-06-28 11:10:12 +0200

    incremental

--- src/Tools/Calendar/TimeCollection.php
+++ src/Tools/Calendar/TimeCollection.php
@@ -14,4 +14,13 @@ class TimeCollection extends \ArrayObject
 
 		return new static($array);
 	}
+
+	public function getUniqueDays(): TimeCollection
+	{
+		return new static(array_map(function (string $date) {
+			return new Time($date);
+		}, array_unique(array_map(function (Time $time) {
+			return $time->format("Y-m-d");
+		}, $this->getArrayCopy()))));
+	}
 }
