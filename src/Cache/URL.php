0ea6264e80ff82d688f718da73b98e8e47c5ae23
Jan Jablunka <jablunka@gmail.com>
2022-07-04 13:06:46 +0200

    incremental

--- src/Cache/URL.php
+++ src/Cache/URL.php
@@ -19,38 +19,38 @@ class URL
 		$this->setCurl($curl);
 	}
 
-	public function setURL(TURL $url) : URL
+	public function setURL(TURL $url): URL
 	{
 		$this->url = $url;
 
 		return $this;
 	}
 
-	public function getURL() : TURL
+	public function getURL(): TURL
 	{
 		return $this->url;
 	}
 
-	public function setTimeout(Timeout $timeout) : URL
+	public function setTimeout(Timeout $timeout): URL
 	{
 		$this->timeout = $timeout;
 
 		return $this;
 	}
 
-	public function getTimeout() : Timeout
+	public function getTimeout(): Timeout
 	{
 		return $this->timeout;
 	}
 
-	public function setCurl(?\Curl\Curl $curl = null) : URL
+	public function setCurl(?\Curl\Curl $curl = null): URL
 	{
 		$this->curl = $curl;
 
 		return $this;
 	}
 
-	public function getCurl() : \Curl\Curl
+	public function getCurl(): \Curl\Curl
 	{
 		if ($this->curl) {
 			return $this->curl;
@@ -62,32 +62,26 @@ class URL
 		return $curl;
 	}
 
-	public function getCallback() : callable
+	public function getCallback(): callable
 	{
 		return function () {
 			$curl = $this->getCurl();
 			$src = $curl->get((string)$this->getURL());
 
 			if (!isset($curl->errorCode)) {
-				throw new \Katu\Exceptions\CacheCallbackException(strtr("Error fetching URL %url%.", [
-					'url' => (string)$this->getURL(),
-				]));
+				throw new \Katu\Exceptions\CacheCallbackException("Error fetching URL \"{$this->getURL()}\".");
 			}
 
 			if ($curl->errorCode) {
-				throw new \Katu\Exceptions\CacheCallbackException(strtr("Error fetching URL %url%, error code %errorCode%, error %error%.", [
-					'%url%' => (string)$this->getURL(),
-					'%errorCode%' => $curl->errorCode,
-					'%error%' => $curl->error,
-				]));
+				throw new \Katu\Exceptions\CacheCallbackException("Error fetching URL \"{$this->getURL()}\", error code {$curl->errorCode}, error \"{$curl->error}\".");
 			}
 
 			$curlInfo = $curl->getInfo();
-			if (!isset($curlInfo['http_code'])) {
+			if (!isset($curlInfo["http_code"])) {
 				throw new \Katu\Exceptions\CacheCallbackException;
 			}
 
-			if ($curlInfo['http_code'] != 200) {
+			if ($curlInfo["http_code"] != 200) {
 				throw new \Katu\Exceptions\CacheCallbackException;
 			}
 
@@ -95,10 +89,17 @@ class URL
 		};
 	}
 
+	public function getGeneralCache(): General
+	{
+		$urlCache = new static($this->getURL(), $this->getTimeout(), $this->getCurl());
+
+		return new General(new TIdentifier("url", $this->getURL()), $this->getTimeout(), $urlCache->getCallback());
+	}
+
 	public static function get(TURL $url, Timeout $timeout, ?\Curl\Curl $curl = null)
 	{
 		$urlCache = new static($url, $timeout, $curl);
-		$cache = new General(new TIdentifier('url', $url), $timeout, $urlCache->getCallback());
+		$cache = new General(new TIdentifier("url", $url), $timeout, $urlCache->getCallback());
 
 		return $cache->getResult();
 	}
