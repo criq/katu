906dfa294fe156a7fd13f5aba00a4b7939e620d7
criq <jablunka@gmail.com>
2022-07-06 14:16:11 +0200

    incremental

--- src/Storage/FileInterface.php
+++ src/Storage/FileInterface.php
@@ -6,11 +6,11 @@ use Katu\Types\TFileSize;
 
 interface FileInterface
 {
+	public function getContent();
 	public function getContentType(): string;
 	public function getExtension(): ?string;
 	public function getFileSize(): TFileSize;
 	public function getName(): string;
 	public function getURI(): string;
-	public function read();
-	public function write(string $content): FileInterface;
+	public function setContent(string $content): FileInterface;
 }
