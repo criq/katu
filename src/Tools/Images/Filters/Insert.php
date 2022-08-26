97085646b4f1a3dbd7fe0ff04e25066454d7a50d
criq <jablunka@gmail.com>
2022-08-26 13:54:47 +0200

    incremental

--- src/Tools/Images/Filters/Insert.php
+++ src/Tools/Images/Filters/Insert.php
@@ -7,7 +7,7 @@ class Insert extends \Katu\Tools\Images\Filter
 	public function apply($image)
 	{
 		$image->insert(
-			(new \Katu\Files\File($this->params["source"]))->getPath(),
+			(new \Katu\Files\File(\App\App::getBaseDir(), $this->params["source"]))->getPath(),
 			isset($this->params["position"]) ? $this->params["position"] : null,
 			isset($this->params["x"]) ? $this->params["x"] : null,
 			isset($this->params["y"]) ? $this->params["y"] : null
