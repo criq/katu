52afd66e636c90d1267ef7b0b53a08dd44bef928
Jan Jablunka <jablunka@gmail.com>
2022-09-05 12:17:09 +0200

    incremental

---
+++ src/Tools/Validation/Rules/IsTruthy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Katu\Tools\Validation\Rules;
+
+use Katu\Errors\Error;
+use Katu\Tools\Validation\Param;
+use Katu\Tools\Validation\Rule;
+use Katu\Tools\Validation\Validation;
+
+class IsTruthy extends Rule
+{
+	public function validate(Param $param): Validation
+	{
+		$validation = new Validation;
+
+		$output = trim($param);
+		if ((bool)$output === true) {
+			$validation->addParam($param->setOutput($output));
+		} else {
+			$validation->addError((new Error($this->getMessage()))->addParam($param));
+		}
+
+		return $validation;
+	}
+}
