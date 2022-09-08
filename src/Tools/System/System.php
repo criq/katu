b6f25c413bf6055b81f5562a36e1bdf86724a3dc
Jan Jablunka <jablunka@gmail.com>
2022-09-08 11:13:00 +0200

    incremental

--- src/Tools/System/System.php
+++ src/Tools/System/System.php
@@ -9,27 +9,27 @@ class System
 {
 	public static function getNumberOfCpus(): int
 	{
-		$cacheIdentifier = new TIdentifier('system', 'numberOfCpus');
+		$cacheIdentifier = new TIdentifier("system", "numberOfCpus");
 
 		return \Katu\Cache\Runtime::get($cacheIdentifier, function () use ($cacheIdentifier) {
-			return \Katu\Cache\General::get($cacheIdentifier, new Timeout('1 day'), function () {
+			return \Katu\Cache\General::get($cacheIdentifier, new Timeout("1 day"), function () {
 				$numCpus = 1;
-				if (is_file('/proc/cpuinfo')) {
-					$cpuinfo = file_get_contents('/proc/cpuinfo');
-					preg_match_all('/^processor/m', $cpuinfo, $matches);
+				if (is_file("/proc/cpuinfo")) {
+					$cpuinfo = file_get_contents("/proc/cpuinfo");
+					preg_match_all("/^processor/m", $cpuinfo, $matches);
 					$numCpus = count($matches[0]);
-				} elseif ('WIN' == strtoupper(substr(PHP_OS, 0, 3))) {
-					$process = @popen('wmic cpu get NumberOfCores', 'rb');
+				} elseif ("WIN" == strtoupper(substr(PHP_OS, 0, 3))) {
+					$process = @popen("wmic cpu get NumberOfCores", "rb");
 					if (false !== $process) {
 						fgets($process);
 						$numCpus = intval(fgets($process));
 						pclose($process);
 					}
 				} else {
-					$process = @popen('sysctl -a', 'rb');
+					$process = @popen("sysctl -a", "rb");
 					if (false !== $process) {
 						$output = stream_get_contents($process);
-						preg_match('/hw.ncpu: (\d+)/', $output, $matches);
+						preg_match("/hw.ncpu: (\d+)/", $output, $matches);
 						if ($matches) {
 							$numCpus = intval($matches[1][0]);
 						}
@@ -66,15 +66,15 @@ class System
 	public static function getDiskSpace()
 	{
 		$res = [];
-		$output = shell_exec('df -kP | awk \'{printf "%-32s\t%16d\t%16d\t%16d\t%16d\t%s\n", $1, $2, $3, $4, $5, $6}\'');
-		foreach (array_values(array_filter(preg_split('/\v/', $output))) as $key => $row) {
+		$output = shell_exec("df -kP | awk \"{printf \"%-32s\t%16d\t%16d\t%16d\t%16d\t%s\n\", $1, $2, $3, $4, $5, $6}\"");
+		foreach (array_values(array_filter(preg_split("/\v/", $output))) as $key => $row) {
 			if ($key > 0) {
-				$array = preg_split('/\t/', $row);
+				$array = preg_split("/\t/", $row);
 				$res[trim($array[0])] = [
-					'filesystem' => trim($array[0]),
-					'mount' => trim($array[5]),
-					'capacity' => new \Katu\Types\TFileSize((int)trim($array[1]), 'kB'),
-					'used' => new \Katu\Types\TFileSize((int)trim($array[2]), 'kB'),
+					"filesystem" => trim($array[0]),
+					"mount" => trim($array[5]),
+					"capacity" => new \Katu\Types\TFileSize((int)trim($array[1]), "kB"),
+					"used" => new \Katu\Types\TFileSize((int)trim($array[2]), "kB"),
 				];
 			}
 		}
