d9126387201ed53b975df9916032a7a44af8f333
Jan Jablunka <jablunka@gmail.com>
2023-02-08 18:42:55 +0100

    incremental

--- src/Tools/Jobs/JobCollection.php
+++ src/Tools/Jobs/JobCollection.php
@@ -11,12 +11,38 @@ use Katu\Types\TIdentifier;
 
 class JobCollection extends \ArrayObject
 {
+	protected $cycleEvery;
+	protected $cycles = 1;
 	protected $lockTimeout;
 	protected $maxRunningSeconds;
 
 	public function getIdentifier(): TIdentifier
 	{
 		return new TIdentifier(static::class);
+	}
+
+	public function setCycles(int $cycles): JobCollection
+	{
+		$this->cycles = $cycles;
+
+		return $this;
+	}
+
+	public function getCycles(): int
+	{
+		return $this->cycles;
+	}
+
+	public function setCycleEvery(?Seconds $cycleEvery): JobCollection
+	{
+		$this->cycleEvery = $cycleEvery;
+
+		return $this;
+	}
+
+	public function getCycleEvery(): Seconds
+	{
+		return $this->cycleEvery ?: new Seconds(10);
 	}
 
 	public function setLockTimeout(Timeout $lockTimeout): JobCollection
@@ -101,6 +127,11 @@ class JobCollection extends \ArrayObject
 
 	public function run()
 	{
-		$this->getProcedure()->run();
+		for ($cycle = 1; $cycle <= $this->getCycles(); $cycle++) {
+			$this->getProcedure()->run();
+			if ($cycle < $this->getCycles()) {
+				sleep($this->getCycleEvery()->getValue());
+			}
+		}
 	}
 }
