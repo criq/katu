7e3d346ae5d3a3b749d9e4e6616afab0a73cbe88
Jan Jablunka <jablunka@gmail.com>
2023-04-04 10:48:02 +0200

    incremental

--- src/PDO/StatementDump.php
+++ src/PDO/StatementDump.php
@@ -26,8 +26,9 @@ class StatementDump
 	public function getSentSQL(): ?string
 	{
 		preg_match("/^(Sent )?SQL: \[[0-9]+\](?<sql>.+)Params:/ms", $this->getSource(), $match);
-		$sql = $match["sql"] ?? null;
-		$sql = preg_replace("\s+", " ", preg_replace("/[\r\n\t]/", " ", $sql));
+		$sql = $match["sql"] ?? "";
+		$sql = preg_replace("/[\r\n\t]/", " ", $sql);
+		$sql = preg_replace("/\s+/", " ", $sql);
 
 		return trim($sql) ?: null;
 	}
