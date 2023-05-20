d1c14010ee957a9da2ec7fcc13bf1597d32a2d46
Jan Jablunka <jablunka@gmail.com>
2023-05-20 12:19:02 +0200

    incremental

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -131,7 +131,7 @@ abstract class Base
 		$sql = SX::select();
 		$sql->from(static::getTable());
 
-		foreach ($where as $name => $value) {
+		foreach ((array)$where as $name => $value) {
 			if ($value instanceof \Sexy\Expression) {
 				$sql->where($value);
 			} elseif (is_null($value)) {
