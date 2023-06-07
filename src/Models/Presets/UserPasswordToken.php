708395b4855805c052f3fce82aaae5d77b7a41de
Jan Jablunka <jablunka@gmail.com>
2023-06-07 17:22:39 +0200

    incremental

--- src/Models/Presets/UserPasswordToken.php
+++ src/Models/Presets/UserPasswordToken.php
@@ -2,38 +2,79 @@
 
 namespace Katu\Models\Presets;
 
+use Katu\Tools\Calendar\Time;
+
 abstract class UserPasswordToken extends \Katu\Models\Model
 {
 	const EXPIRES = "1 hour";
 	const TABLE = "user_password_tokens";
 
-	public static function create(User $user)
+	public $timeExpires;
+	public $timeUsed;
+	public $token;
+	public $userId;
+
+	public static function create(User $user): UserPasswordToken
 	{
 		return static::insert([
-			"timeCreated" => new \Katu\Tools\Calendar\Time,
-			"timeExpires" => new \Katu\Tools\Calendar\Time(static::EXPIRES),
+			"timeCreated" => new Time,
+			"timeExpires" => new Time(static::EXPIRES),
 			"userId" => $user->getId(),
-			"token" => \Katu\Tools\Random\Generator::getString(static::getColumn("token")->getDescription()->length),
+			"token" => static::generateToken(),
 		]);
 	}
 
-	public function getUser()
+	public static function generateToken(): string
+	{
+		return \Katu\Tools\Random\Generator::getString(static::getColumn("token")->getDescription()->length);
+	}
+
+	public function getUser(): User
 	{
 		$class = \App\App::getContainer()->get(\Katu\Models\Presets\User::class);
 
 		return $class::get($this->userId);
 	}
 
-	public function isValid()
+	public function setTimeExpires(Time $time): UserPasswordToken
 	{
-		return (new \Katu\Tools\Calendar\Time($this->timeExpires))->isInFuture() && !$this->timeUsed;
+		$this->timeExpires = $time;
+
+		return $this;
 	}
 
-	public function expire()
+	public function getTimeExpires(): Time
 	{
-		$this->timeUsed = (new \Katu\Tools\Calendar\Time)->getDbDateTimeFormat();
+		return new Time($this->timeExpires);
+	}
+
+	public function setTimeUsed(?Time $time): UserPasswordToken
+	{
+		$this->timeUsed = $time;
+
+		return $this;
+	}
+
+	public function getTimeUsed(): ?Time
+	{
+		return $this->timeUsed ? new Time($this->timeUsed) : null;
+	}
+
+	public function getToken(): string
+	{
+		return $this->token;
+	}
+
+	public function getIsValid(): bool
+	{
+		return $this->getTimeExpires()->isInFuture() && !$this->getTimeUsed();
+	}
+
+	public function expire(): UserPasswordToken
+	{
+		$this->setTimeUsed(new Time);
 		$this->save();
 
-		return true;
+		return $this;
 	}
 }
