0e59ea368b690c937076245339234f25a0f03522
Jan Jablunka <jablunka@gmail.com>
2023-08-02 20:42:14 +0200

    incremental

--- src/Tools/Session/Flash.php
+++ src/Tools/Session/Flash.php
@@ -2,6 +2,13 @@
 
 namespace Katu\Tools\Session;
 
+use Katu\Tools\Strings\Code;
+use Katu\Types\TClass;
+
 abstract class Flash
 {
+	public function getClassCode(): Code
+	{
+		return new Code((new TClass($this))->getShortName());
+	}
 }
--- src/Tools/Session/FlashCollection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Katu\Tools\Session;
-
-class FlashCollection extends \ArrayObject
-{
-}
