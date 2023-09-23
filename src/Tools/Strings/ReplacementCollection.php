05f1bb4b6470aac0237a6bad1cb83759892ca768
Jan Jablunka <jablunka@gmail.com>
2023-09-23 11:08:46 +0200

    incremental

--- src/Tools/Strings/ReplacementCollection.php
+++ src/Tools/Strings/ReplacementCollection.php
@@ -2,15 +2,45 @@
 
 namespace Katu\Tools\Strings;
 
+use App\Classes\Users\RepresentativeCollection;
+use Katu\Tools\Views\ArrayLoaderTwigEngine;
+
 class ReplacementCollection extends \ArrayObject
 {
-	public function apply(string $input): string
+	public static function createFromArray(array $array)
 	{
-		$output = $input;
-		foreach ($this as $replacement) {
-			$output = preg_replace($replacement->getKeyRegex(), $replacement->getValue(), $output);
+		$replacements = new static;
+
+		foreach ($array as $key => $value) {
+			$replacements[] = new Replacement(new Code($key), $value);
 		}
 
-		return $output;
+		return $replacements;
+	}
+
+	public static function createGlobal(): ReplacementCollection
+	{
+		return (new static)
+			->mergeWith(RepresentativeCollection::createFromConfig()->getReplacements())
+			;
+	}
+
+	public function mergeWith(ReplacementCollection $replacements): ReplacementCollection
+	{
+		return new static(array_merge($this->getArrayCopy(), $replacements->getArrayCopy()));
+	}
+
+	public function render(string $template): string
+	{
+		return (string)ArrayLoaderTwigEngine::renderString($template, $this->getArray());
+	}
+
+	public function getArray(): array
+	{
+		return array_combine(array_map(function (Replacement $replacement) {
+			return $replacement->getCode()->getConstantFormat();
+		}, $this->getArrayCopy()), array_map(function (Replacement $replacement) {
+			return $replacement->getValue();
+		}, $this->getArrayCopy()));
 	}
 }
