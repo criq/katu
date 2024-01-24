97de5cc4706605483d00ef2bb583ba0456750d4d
Jan Jablunka <jablunka@gmail.com>
2024-01-24 16:40:34 +0100

    incremental

--- src/Controllers/Presets/Images.php
+++ src/Controllers/Presets/Images.php
@@ -14,50 +14,11 @@ class Images extends \Katu\Controllers\Controller
 		$version = \Katu\Tools\Images\Version::createFromConfig($versionCode);
 
 		$imageVersion = $image->getImageVersion($version);
-		$imageVersion->getVersionImage();
 
 		return $response
 			->withHeader("Content-Type", $imageVersion->getFile()->getMime())
 			->withHeader("Cache-Control", "max-age=604800, public")
 			->withBody(\GuzzleHttp\Psr7\Utils::streamFor($imageVersion->getFile()->get()))
 			;
-
-	}
-
-	/**
-	 * @deprecated
-	*/
-	public static function getVersionSrcFile(ServerRequestInterface $request, ResponseInterface $response, string $fileId, string $fileSecret, string $version)
-	{
-		try {
-			$fileClass = \App\App::getContainer()->get(\Katu\Models\Presets\File::class);
-
-			$file = $fileClass::getOneBy([
-				"id" => $fileId,
-				"secret" => $fileSecret,
-			]);
-
-			if (!$file) {
-				throw new \Katu\Exceptions\ModelNotFoundException;
-			}
-
-			try {
-				$version = \Katu\Tools\Images\Version::createFromConfig($version);
-			} catch (\Katu\Exceptions\MissingConfigException $e) {
-				throw new \Katu\Exceptions\NotFoundException;
-			}
-
-			$image = new \Katu\Tools\Images\Image($file);
-			$imageVersion = $image->getImageVersion($version);
-			$imageVersion->getVersionImage();
-
-			return $response
-				->withHeader("Content-Type", $imageVersion->getFile()->getMime())
-				->withHeader("Cache-Control", "max-age=604800, public")
-				->withBody(\GuzzleHttp\Psr7\Utils::streamFor($imageVersion->getFile()->get()))
-				;
-		} catch (\Throwable $e) {
-			throw new \Katu\Exceptions\NotFoundException;
-		}
 	}
 }
