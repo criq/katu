27adb55a39c94a500ae447081cb054074fdd9a9d
Jan Jablunka <jablunka@gmail.com>
2024-03-07 11:50:28 +0100

    incremental

--- src/Utils/YAML.php
+++ src/Utils/YAML.php
@@ -2,23 +2,25 @@
 
 namespace Katu\Utils;
 
-class YAML {
-
-	static function respond($var) {
+class YAML
+{
+	static function respond($var)
+	{
 		$app = \Katu\App::get();
 
-		$app->response->headers->set('Content-Type', 'text/plain; charset=UTF-8');
+		$app->response->headers->set("Content-Type", "text/plain; charset=UTF-8");
 		$app->response->setBody(self::encode($var));
 
 		return true;
 	}
 
-	static function encode($var) {
+	static function encode($var)
+	{
 		return \Spyc::YAMLDump($var);
 	}
 
-	static function decode($var) {
+	static function decode($var)
+	{
 		return \Spyc::YAMLLoad($var);
 	}
-
 }
