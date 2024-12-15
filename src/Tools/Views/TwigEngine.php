81d50f4119a9b909b6721d66485079e407e88fa6
Jan Jablunka <jablunka@gmail.com>
2024-12-15 16:36:53 +0100

    incremental

--- src/Tools/Views/TwigEngine.php
+++ src/Tools/Views/TwigEngine.php
@@ -343,17 +343,16 @@ abstract class TwigEngine implements ViewEngineInterface
 		return $data;
 	}
 
-	public function render(string $template, array $data = []): StreamInterface
+	public function getTemplate(string $template, array $data = []): ?string
 	{
 		$twig = $this->getTwig();
 		$data = array_merge_recursive($this->getCommonData(), $data);
 
-		try {
-			return \GuzzleHttp\Psr7\Utils::streamFor(trim($twig->render($template, $data)));
-		} catch (\Throwable $e) {
-			\App\App::getLogger(new TIdentifier(__CLASS__, __FUNCTION__))->error($e);
+		return $twig->render($template, $data);
+	}
 
-			throw $e;
-		}
+	public function render(string $template, array $data = []): StreamInterface
+	{
+		return \GuzzleHttp\Psr7\Utils::streamFor($this->getTemplate($template, $data));
 	}
 }
