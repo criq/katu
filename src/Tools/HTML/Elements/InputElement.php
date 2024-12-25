b6c5c914c9f90737ec1804e6da8cea8cf5c250a5
Jan Jablunka <jablunka@gmail.com>
2024-12-25 19:54:34 +0100

    incremental

---
+++ src/Tools/HTML/Elements/InputElement.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Katu\Tools\HTML\Elements;
+
+use Katu\Tools\HTML\Attribute;
+use Katu\Tools\HTML\ElementNode;
+
+class InputElement extends ElementNode
+{
+	public function __construct(?string $type = null, ?string $name = null, ?string $value = null, ?string $id = null)
+	{
+		$this->setName("input");
+		$this->setAttribute(new Attribute("type", $type));
+		$this->setAttribute(new Attribute("name", $name));
+		$this->setAttribute(new Attribute("value", $value));
+		$this->setAttribute(new Attribute("id", $id));
+	}
+}
