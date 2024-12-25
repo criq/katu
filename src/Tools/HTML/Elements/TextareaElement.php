3cd380b4219464f9005ed4a9dd447b6a053da832
Jan Jablunka <jablunka@gmail.com>
2024-12-25 20:45:14 +0100

    incremental

---
+++ src/Tools/HTML/Elements/TextareaElement.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Katu\Tools\HTML\Elements;
+
+use Katu\Tools\HTML\Attribute;
+use Katu\Tools\HTML\NodeCollection;
+use Katu\Tools\HTML\TextNode;
+
+class TextareaElement extends InputElement
+{
+	public function __construct(?string $name = null, ?string $value = null, ?string $id = null)
+	{
+		$this->setName("textarea");
+		$this->setAttribute(new Attribute("name", $name));
+		$this->setAttribute(new Attribute("id", $id));
+		$this->setNodes(new NodeCollection([
+			new TextNode($value),
+		]));
+	}
+}
