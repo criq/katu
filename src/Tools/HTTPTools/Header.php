c7606f4718dd7ee2058befab2a9ab16880fd7f80
Jan Jablunka <jablunka@gmail.com>
2025-02-03 14:32:23 +0100

    incremental

--- src/Tools/HTTPTools/Header.php
+++ src/Tools/HTTPTools/Header.php
@@ -20,10 +20,30 @@ class Header
 		return $this;
 	}
 
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public static function getComparableName(string $name): string
+	{
+		return mb_strtoupper(trim($name));
+	}
+
+	public function hasName(string $name): bool
+	{
+		return $this->getComparableName($name) == $this->getComparableName($this->getName());
+	}
+
 	public function setValue(?string $value): Header
 	{
 		$this->value = $value;
 
 		return $this;
+	}
+
+	public function getValue(): ?string
+	{
+		return $this->value;
 	}
 }
