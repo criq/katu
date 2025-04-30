49d76cfa30543eed3a5ff4a60dc19367084f314b
Jan Jablunka <jablunka@gmail.com>
2025-04-30 10:57:09 +0200

    Add description property to Replacement class

--- src/Tools/Strings/Replacement.php
+++ src/Tools/Strings/Replacement.php
@@ -5,12 +5,14 @@ namespace Katu\Tools\Strings;
 class Replacement
 {
 	protected $code;
+	protected $description;
 	protected $value;
 
-	public function __construct(Code $code, ?string $value)
+	public function __construct(Code $code, ?string $value, ?string $description = null)
 	{
 		$this->setCode($code);
 		$this->setValue($value);
+		$this->setDescription($description);
 	}
 
 	public function setCode(Code $code): Replacement
@@ -35,5 +37,17 @@ class Replacement
 	public function getValue(): ?string
 	{
 		return $this->value;
+	}
+
+	public function setDescription(?string $description): Replacement
+	{
+		$this->description = $description;
+
+		return $this;
+	}
+
+	public function getDescription(): ?string
+	{
+		return $this->description;
 	}
 }
