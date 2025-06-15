d362cd104e4cebe2e207cee3acbacac56d537fa0
Jan Jablunka <jablunka@gmail.com>
2025-06-15 14:49:09 +0200

    Add getAssoc method to ImageVersionCollection and update getRestResponse to use it

--- src/Tools/Images/ImageVersionCollection.php
+++ src/Tools/Images/ImageVersionCollection.php
@@ -9,10 +9,20 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class ImageVersionCollection extends \ArrayObject implements RestResponseInterface
 {
+	public function getAssoc(): ImageVersionCollection
+	{
+		return new static(array_combine(
+			array_map(function (ImageVersion $imageVersion) {
+				return $imageVersion->getVersion()->getTitle();
+			}, $this->getArrayCopy()),
+			array_values($this->getArrayCopy()),
+		));
+	}
+
 	public function getRestResponse(?ServerRequestInterface $request = null, ?OptionCollection $options = null): RestResponse
 	{
 		return new RestResponse(array_map(function (ImageVersion $imageVersion) use ($request, $options) {
 			return $imageVersion->getRestResponse($request, $options);
-		}, $this->getArrayCopy()));
+		}, $this->getAssoc()->getArrayCopy()));
 	}
 }
