39ec6d906ef47526eedd0b8fe8c768cd2d6d6af5
Jan Jablunka <jablunka@gmail.com>
2025-08-29 14:15:06 +0200

    Update QRCode.php

--- src/Tools/Images/QRCode.php
+++ src/Tools/Images/QRCode.php
@@ -68,16 +68,16 @@ class QRCode
 
 	public function getImageString(\Endroid\QrCode\Writer\WriterInterface $writer): string
 	{
-		return \Endroid\QrCode\Builder\Builder::create()
-			->writer($writer)
-			->data($this->getString())
-			->encoding(new \Endroid\QrCode\Encoding\Encoding("UTF-8"))
-			->size($this->getResolvedSize())
-			->margin($this->getResolvedMargin())
-			->roundBlockSizeMode(new \Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin)
-			->build()
-			->getString()
-			;
+		$qrCode = new \Endroid\QrCode\QrCode(
+			$this->getString(),
+			new \Endroid\QrCode\Encoding\Encoding("UTF-8"),
+			\Endroid\QrCode\ErrorCorrectionLevel::Low,
+			$this->getResolvedSize(),
+			$this->getResolvedMargin(),
+			\Endroid\QrCode\RoundBlockSizeMode::Margin
+		);
+
+		return $writer->write($qrCode)->getString();
 	}
 
 	public function getPNG(): string
